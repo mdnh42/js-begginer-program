48a842279642f4dc39e1dd358080fa6095335ff5
mdnh42 <mdnh.42@gmail.com>
2021-08-06 01:06:28 +0600

    Two number sum

---
+++ program4.js
@@ -0,0 +1,9 @@
+// Sum two number
+
+var number1 = 50; // Declare first number 
+var number2 = 60;// Declare second number 
+var sum = number1 + number2; // stored the two number in sum variable
+
+console.log("50 + 60 = ", sum); // print the result
+
+// Thanks 
