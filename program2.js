c0f30bcac4e6475fd37b5b3e79ffb6026e360e16
mdnh42 <mdnh.42@gmail.com>
2021-08-06 00:57:09 +0600

    secod program number

---
+++ program2.js
@@ -0,0 +1,7 @@
+// Your second program with javaScript;
+// Print number string; 
+
+var number = 12343; // Declare a variable which name is number and stored some digit. 
+console.log(number); // print the Number with console.log();
+
+//Thanks
