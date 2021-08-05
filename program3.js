59d70a96f27d3a08e2786f31dbca82d63bb0591a
mdnh42 <mdnh.42@gmail.com>
2021-08-06 01:01:40 +0600

    Boolean data types check program

---
+++ program3.js
@@ -0,0 +1,12 @@
+// Your third program with javaScript;
+// looks true or false; 
+
+var pass = true; // Declare a varible which it a boolean types here i meaning it pass is true; 
+var email = false;  // Decalare a second variable which it a boolean types value here i meaning that email is not correct. 
+
+console.log(pass, email); // print the pass & email with console.log();
+// you can write this types
+console.log('My Password is', pass);
+console.log('my email is', email);
+
+//Thanks for watcing this code; 
