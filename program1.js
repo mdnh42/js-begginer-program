694373bfebc066431940a51bf383d797748e7c19
mdnh42 <mdnh.42@gmail.com>
2021-08-06 00:52:39 +0600

    JavaScript Program for begginer

---
+++ program1.js
@@ -0,0 +1,7 @@
+// Your First program with javaScript;
+// Print Some string; 
+
+var someString = "Hello Everyone. I'm MD. Nur Hasan." // Declare a variable which name is someString and stored some text within qoutetaion. 
+console.log(someString); // print the someString with console.log();
+
+//Thanks
